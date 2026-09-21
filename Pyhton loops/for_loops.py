053dc2d335ac7a298878750c65c8867bafc278f6
Hussein Adebayo <hellohusseinadebayo@gmail.com>
2026-09-21 17:06:33 +0100

    https://github.com/hussein-automator/pythonLearning.git git config --global user.email "hellohusseinadebayo@gmail.com" git config --global user.name "Hussein Adebayo"

---
+++ Pyhton loops/for_loops.py
@@ -0,0 +1,25 @@
+"""numbers = range(1, 1000)
+total = 0
+
+for num in numbers:
+    if (num % 3 == 0) or (num % 5 == 0):
+        total += num
+        
+print(f"Multiple of 3 or 5 = {total}")
+"""
+square_total = 0
+numbers = range(1, 787000)
+
+for num in numbers:
+    # Find even numbers
+    even_num = num % 2 == 0
+    
+    # Get odd numbers
+    if not even_num:
+        odd_num = num
+        square_num = odd_num ** 2
+        square_total += square_num
+
+print(square_total)
+        
+   
